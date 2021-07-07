4dc81939aff060473462cb5eeab7ad4454dddae2
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-07 14:02:01 -0700

    materialUI styles for front-end started

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
-
 import grams from './images/grams.jpeg'
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">chrisgram</Typography>
-        <img src={grams} alt="grams" height="800" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">chrisgram</Typography>
+        <img className={classes.image} src={grams} alt="grams" height="200" />
 
       </AppBar>
       <Grow in>
