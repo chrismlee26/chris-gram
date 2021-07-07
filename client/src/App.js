6535d7bd47732e2d1970a6a1be2d9064ac40b7b5
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-07 13:17:18 -0700

    posts & front end placeholders connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+
+import grams from './images/grams.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">chrisgram</Typography>
+        <img src={grams} alt="grams" height="800" />
+
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
