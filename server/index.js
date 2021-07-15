644f5cb31c9858ab955d36fca1ea56c995f2c53c
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-15 08:34:19 -0700

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Chrisgram')
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { 
